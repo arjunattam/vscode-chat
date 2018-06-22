e061c8606fd7c5c3331c6e90a73ff87ac75ce942
Arjun Attam <arjunattam@gmail.com>
2018-06-22 17:30:02 +0530

    Send and receive slack messages from webview

--- src/extension.ts
+++ src/extension.ts
@@ -4,30 +4,18 @@ import * as vscode from "vscode";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "vscode-rubberduck" is now active!'
-  );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand("extension.sayHello", () => {
     // The code you place here will be executed every time your command is executed
-    vscode.window.showInformationMessage("Hello World!");
     const panel = vscode.window.createWebviewPanel(
       "catCoding", // Identifies the type of the webview. Used internally
       "Cat Coding", // Title of the panel displayed to the user
-      vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
-      {} // Webview options. More on these later.
+      vscode.ViewColumn.Three,
+      { enableScripts: true }
     );
 
     let messages = [];
 
     const updateWebview = () => {
-      console.log(messages);
-      console.log(getWebviewContent(messages));
       panel.webview.html = getWebviewContent(messages);
     };
 
@@ -36,17 +24,6 @@ export function activate(context: vscode.ExtensionContext) {
 
     const token =
       "xoxp-282186700213-282087778692-377864597989-88a83d1b455fcf664f0fc8ca8cfcc7c9";
-
-    //     const { WebClient } = require("@slack/client");
-    //     const web = new WebClient(token);
-    //     web.channels
-    //       .list()
-    //       .then(res => {
-    //         // `res` contains information about the channels
-    //         res.channels.forEach(c => console.log(c));
-    //       })
-    //       .catch(console.error);
-
     const conversationId = "CBC6RU92P";
 
     const { RTMClient } = require("@slack/client");
@@ -54,18 +31,31 @@ export function activate(context: vscode.ExtensionContext) {
     rtm.start();
     rtm.on("message", event => {
       // Structure of `event`: <https://api.slack.com/events/message>
-      console.log(`Message from ${event.user}: ${event.text}`);
-      messages.push(event.text);
+
+      // TODO(arjun): maintain users list
+      messages.push(`${event.user}: ${event.text}`);
       updateWebview();
     });
 
-    rtm
-      .sendMessage("Hello there", conversationId)
-      .then(res => {
-        // `res` contains information about the posted message
-        console.log("Message sent: ", res.ts);
-      })
-      .catch(console.error);
+    // Handle messages from the webview
+    panel.webview.onDidReceiveMessage(
+      message => {
+        switch (message.command) {
+          case "send":
+            rtm
+              .sendMessage(message.text, conversationId)
+              .then(result => {
+                messages.push(message.text);
+                updateWebview();
+                console.log("Message sent: ", result.ts);
+              })
+              .catch(console.error);
+            return;
+        }
+      },
+      undefined,
+      context.subscriptions
+    );
   });
 
   context.subscriptions.push(disposable);
@@ -90,6 +80,23 @@ function getWebviewContent(messages) {
     <ul>
         ${messagesItems}
     </ul>
+    <form id="submit-form">
+        <input type="text" id="message-input" name="message" value="Enter message">
+        <input type="submit" value="Submit">
+    </form>
+
+    <script>
+        (function() {
+            const vscode = acquireVsCodeApi();
+            document.getElementById('submit-form').addEventListener('submit', function(e) {
+                e.preventDefault(); //to prevent form submission
+                vscode.postMessage({
+                    command: 'send',
+                    text: document.getElementById('message-input').value
+                })
+            });
+        }())
+    </script>
 </body>
 </html>`;
 }
