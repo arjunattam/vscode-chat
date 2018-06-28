a0170fd6458f33f281d7857b87d46a66235b4c81
Arjun Attam <arjunattam@gmail.com>
2018-06-29 00:05:30 +0530

    Merged branch

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,23 @@
 export const EXTENSION_ID = "karigari.chat";
 export const OUTPUT_CHANNEL_NAME = "Slack Chat";
+export const APP_INSIGHTS_KEY = "ac30cb4c-9282-4947-8652-4a0ac828f0ce";
+
+export const LiveShareCommands = {
+  START: "liveshare.start",
+  END: "liveshare.end",
+  JOIN: "liveshare.join"
+};
+
+export const VSCodeCommands = {
+  OPEN: "vscode.open"
+};
+
+export const COMMAND_ACTIONS = {
+  live: {
+    share: {
+      action: LiveShareCommands.START,
+      options: { suppressNotification: true }
+    },
+    end: { action: LiveShareCommands.END, options: {} }
+  }
+};
