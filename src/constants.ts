89165a165acdbc3e64ed9216a368e0e57332ca18
Arjun Attam <arjunattam@gmail.com>
2019-03-12 15:34:22 -0700

    Update constants

--- src/constants.ts
+++ src/constants.ts
@@ -19,6 +19,8 @@ export const VSCodeCommands = {
   OPEN_SETTINGS: "workbench.action.openSettings"
 };
 
+export const ONE_MORE_CONSTANT = 3.1428;
+
 export const SelfCommands = {
   OPEN_WEBVIEW: "extension.chat.openChatPanel",
   CHANGE_WORKSPACE: "extension.chat.changeWorkspace",
