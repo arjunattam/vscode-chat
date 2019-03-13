1e4e709207581c8a5d2bf829e1719fd388e65150
Arjun Attam <arjunattam@gmail.com>
2019-03-12 20:14:25 -0700

    another constant

--- src/constants.ts
+++ src/constants.ts
@@ -20,7 +20,7 @@ export const VSCodeCommands = {
 };
 
 export const ONE_MORE_CONSTANT = 3.1428;
-
+const ANOTHER = 2.72;
 export const SelfCommands = {
   OPEN_WEBVIEW: "extension.chat.openChatPanel",
   CHANGE_WORKSPACE: "extension.chat.changeWorkspace",
